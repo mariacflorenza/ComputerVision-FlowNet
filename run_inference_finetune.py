d05b9cd3dd085d2ddd2d8cadd397fc6025f44fec
ThanhNhanNGUYEN0401 <167876624+ThanhNhanNGUYEN0401@users.noreply.github.com>
2025-03-20 17:40:10 +0100

    Run inference for fine-tuned model

---
+++ run_inference_finetune.py
@@ -0,0 +1,231 @@
+import argparse
+from path import Path
+
+import torch
+import torch.backends.cudnn as cudnn
+import torch.nn.functional as F
+import models
+from tqdm import tqdm
+
+import torchvision.transforms as transforms
+import flow_transforms
+from imageio.v2 import imread, imwrite
+import numpy as np
+from util import flow2rgb
+import cv2
+from util import create_img_pairs
+
+from models.FlowNetS import FlowNetS  # Import FlowNetS directly
+from models.FlowNetC import FlowNetC  # Import FlowNetC if needed
+
+# import imageio.v2 as imageio
+
+model_names = sorted(
+    name for name in models.__dict__ if name.islower() and not name.startswith("__")
+)
+
+
+parser = argparse.ArgumentParser(
+    description="PyTorch FlowNet inference on a folder of img pairs",
+    formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+)
+parser.add_argument(
+    "data",
+    metavar="DIR",
+    help="path to images folder, image names must match '[name]0.[ext]' and '[name]1.[ext]'",
+)
+parser.add_argument("pretrained", metavar="PTH", help="path to pre-trained model")
+
+# select sequence # MF
+parser.add_argument(
+    "--sequence",
+    "-s",
+    metavar="PREFIX",
+    default=None,
+    help="Prefix for image sequences (e.g., 'bear' for bear-001, bear-002, ...)",
+)
+parser.add_argument(
+    "--output",
+    "-o",
+    metavar="DIR",
+    default=None,
+    help="path to output folder. If not set, will be created in data folder",
+)
+parser.add_argument(
+    "--output-value",
+    "-v",
+    choices=["raw", "vis", "both"],
+    default="both",
+    help="which value to output, between raw input (as a npy file) and color vizualisation (as an image file)."
+    " If not set, will output both",
+)
+parser.add_argument(
+    "--div-flow",
+    default=20,
+    type=float,
+    help="value by which flow will be divided. overwritten if stored in pretrained file",
+)
+parser.add_argument(
+    "--img-exts",
+    metavar="EXT",
+    default=["png", "jpg", "bmp", "ppm"],
+    nargs="*",
+    type=str,
+    help="images extensions to glob",
+)
+parser.add_argument(
+    "--max_flow",
+    default=None,
+    type=float,
+    help="max flow value. Flow map color is saturated above this value. If not set, will use flow map's max value",
+)
+parser.add_argument(
+    "--upsampling",
+    "-u",
+    choices=["nearest", "bilinear"],
+    default=None,
+    help="if not set, will output FlowNet raw input,"
+    "which is 4 times downsampled. If set, will output full resolution flow map, with selected upsampling",
+)
+parser.add_argument(
+    "--bidirectional",
+    action="store_true",
+    help="if set, will output invert flow (from 1 to 0) along with regular flow",
+)
+
+device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+
+
+@torch.no_grad()
+def main():
+    global args, save_path
+    args = parser.parse_args()
+
+    if args.output_value == "both":
+        output_string = "raw output and RGB visualization"
+    elif args.output_value == "raw":
+        output_string = "raw output"
+    elif args.output_value == "vis":
+        output_string = "RGB visualization"
+    print("=> will save " + output_string)
+    data_dir = Path(args.data)
+    print("=> fetching img pairs in '{}'".format(args.data))
+    if args.output is None:
+        save_path = data_dir / "flow"
+    else:
+        save_path = Path(args.output)
+    print("=> will save everything to {}".format(save_path))
+    save_path.makedirs_p()
+    # Data loading code
+    input_transform = transforms.Compose(
+        [
+            flow_transforms.ArrayToTensor(),
+            transforms.Normalize(mean=[0, 0, 0], std=[255, 255, 255]),
+            transforms.Normalize(mean=[0.411, 0.432, 0.45], std=[1, 1, 1]),
+        ]
+    )
+
+    # img_pairs = []
+    # for ext in args.img_exts:
+    #     test_files = data_dir.files("*1.{}".format(ext))
+    #     for file in test_files:
+    #         img_pair = file.parent / (file.stem[:-1] + "2.{}".format(ext))
+    #         if img_pair.is_file():
+    #             print("adding file :", file, img_pair)
+    #             img_pairs.append([file, img_pair])
+
+    img_pairs = create_img_pairs(data_dir, args.sequence)
+    print("image pairs !!! ", img_pairs)
+
+    print("{} samples found".format(len(img_pairs)))
+    # create model
+    # network_data = torch.load(args.pretrained)
+    network_data = torch.load(args.pretrained, map_location=torch.device('cpu'))
+
+
+    # Manually specify the architecture
+    model_arch = "FlowNetS"  # Replace "FlowNetS" with "FlowNetC" if needed
+
+    print(f"=> using pre-trained model '{model_arch}'")
+    if model_arch == "FlowNetS":
+        model = FlowNetS(batchNorm=False).to(device)  # Initialize FlowNetS
+    elif model_arch == "FlowNetC":
+        model = FlowNetC(batchNorm=False).to(device)  # Initialize FlowNetC
+    else:
+        raise ValueError(f"Unknown model architecture: {model_arch}")
+
+    model.load_state_dict(network_data)  # Load the weights
+    model.eval()
+    cudnn.benchmark = True
+
+    if "div_flow" in network_data.keys():
+        args.div_flow = network_data["div_flow"]
+
+    for img1_file, img2_file in tqdm(img_pairs):
+
+        # preprocessing of images 
+        img1 = cv2.imread(img1_file)
+        img2 = cv2.imread(img2_file)
+        sigma_s = 60
+        sigma_r = 0.3
+        # print("Pretreatment of images !!!")
+
+        img1 = cv2.GaussianBlur(img1,(5,5),cv2.BORDER_DEFAULT)
+        img2 = cv2.GaussianBlur(img2,(5,5),cv2.BORDER_DEFAULT)
+
+        img1 = cv2.resize(img1, (512, 384))
+        img2 = cv2.resize(img2, (512, 384))
+
+        # img1 = cv2.edgePreservingFilter(img1, flags=1, sigma_s=sigma_s, sigma_r=sigma_r)
+        # img2 = cv2.edgePreservingFilter(img2, flags=1, sigma_s=sigma_s, sigma_r=sigma_r)
+
+        # img1 = cv2.detailEnhance(img1, sigma_s=sigma_s, sigma_r=sigma_r)
+        # img2 = cv2.detailEnhance(img2, sigma_s=sigma_s, sigma_r=sigma_r)
+        
+        # img1 = cv2.pencilSketch(img1, sigma_s=sigma_s, sigma_r=sigma_r)
+        # img2 = cv2.pencilSketch(img2, sigma_s=sigma_s, sigma_r=sigma_r)
+
+        
+        img1 = input_transform(img1)
+        img2 = input_transform(img2)
+
+        # img1 = input_transform(imread(img1_file))
+        # img2 = input_transform(imread(img2_file))
+        input_var = torch.cat([img1, img2]).unsqueeze(0)
+
+        if args.bidirectional:
+            # feed inverted pair along with normal pair
+            inverted_input_var = torch.cat([img2, img1]).unsqueeze(0)
+            input_var = torch.cat([input_var, inverted_input_var])
+
+        input_var = input_var.to(device)
+        # compute output
+        output = model(input_var)
+        if args.upsampling is not None:
+            output = F.interpolate(
+                output, size=img1.size()[-2:], mode=args.upsampling, align_corners=False
+            )
+        for suffix, flow_output in zip(["flow", "inv_flow"], output):
+            # filename = save_path / "{}{}".format(img1_file.stem[:-1], suffix)
+            # MF
+            filename = save_path / "{}-{}-{}-{}".format( 
+                img1_file.stem.split("-")[0],  # Parte común de la imagen, por ejemplo "bear"
+                suffix,
+                img1_file.stem.split("-")[1],  # Número de la primera imagen, por ejemplo "001"
+                img2_file.stem.split("-")[1],  # Número de la segunda imagen, por ejemplo "002"
+            )
+            # print(filename)
+            if args.output_value in ["vis", "both"]:
+                rgb_flow = flow2rgb(
+                    args.div_flow * flow_output, max_value=args.max_flow
+                )
+                to_save = (rgb_flow * 255).astype(np.uint8).transpose(1, 2, 0)
+                imwrite(filename + ".png", to_save)
+            if args.output_value in ["raw", "both"]:
+                # Make the flow map a HxWx2 array as in .flo files
+                to_save = (args.div_flow * flow_output).cpu().numpy().transpose(1, 2, 0)
+                np.save(filename + ".npy", to_save)
+
+
+if __name__ == "__main__":
+    main()
