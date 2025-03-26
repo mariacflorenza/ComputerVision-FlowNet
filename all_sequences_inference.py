e7befbe3603cfe75d0ec820b80814ea76bb9b026
mariacflorenza <maria.florenza-lamberti@imt-atlantique.net>
2025-03-26 09:51:28 +0100

    all sequences for test

--- all_sequences_inference.py
+++ all_sequences_inference.py
@@ -2,21 +2,28 @@ import os
 import subprocess
 import argparse
 import run_inference
-# $ python3 all_sequences_inference.py --pretrained flownets_EPE1.951.pth --mode direct/sequential
+# $ python3 all_sequences_inference.py --pretrained flownets_EPE1.951.pth --mode direct/sequential --sequence train/test
 # Pretrained model path from the arguments
 parser = argparse.ArgumentParser(description="Run inference for all sequences using FlowNet")
 parser.add_argument('--pretrained', type=str, required=True, help='Path to the pre-trained model')
 parser.add_argument('--mode', type=str, default='sequential', choices=['sequential', 'direct', 'inference'],
                         help='Choose mode: complete_inferece_saving_seq, inference_direct, or inference')
+parser.add_argument('--sequence', type=str, default='train', choices=['train', 'test'],
+                        help='Choose sequence: train or test')
 args = parser.parse_args()
+if args.sequence == 'train':
+    print(f"Running inference for the {args.sequence} sequence")
+    image_folder = 'sequences-train'
+    sequence_list = ['bear', 'book', 'bag', 'camel', 'rhino', 'swan']
+else:
+    print(f"Running inference for the {args.sequence} sequence")
+    image_folder = 'sequences-test'
+    sequence_list = ['cow', 'fish', 'octopus']
 
-sequence_list = ['bear', 'book', 'bag', 'camel', 'rhino', 'swan']
-
-run_inference_script = 'run_inference_test.py'
+run_inference_script = 'run_inference.py'
 
 pretrained_model_path = args.pretrained
 
-image_folder = 'sequences-train'
 
 for sequence in sequence_list:
     # image_folder = os.path.join(image_folder_base, sequence)
